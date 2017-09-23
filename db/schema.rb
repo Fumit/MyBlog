4539330674f5ba159cad802b8306acee7e6f0ac0
Fumit <fumi.rugby2316@gmail.com>
2017-09-23 16:02:34 +0000

    Add name to view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922022134) do
+ActiveRecord::Schema.define(version: 20170923153413) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
